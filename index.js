3b58776f753cb2b8c4a08a8dff92c18a03f3e58a
Fábio Almeida <fabio_almeida100@hotmail.com>
2020-08-01 13:47:15 -0300

    5 states with more cities

--- index.js
+++ index.js
@@ -1,33 +1,53 @@
-import { promises as fs } from "fs";
-import { Console } from "console";
+const fs = require('fs'); 
 
 const _UF = process.argv[2].trim();
 const folderCities = "cities-state/";
+let _states = [];
 
 init();
 
-async function  init() {
+function init() {
    createFileCity();
-   const countCitiesOfState = await countCities();
+   const countCitiesOfState = countCities(_UF);
+   const countCitiesByState = GetArrayCountCitiesOfStates();
 
-   console.log(`A UF: ${_UF} tem ${countCitiesOfState} cidades`);
+   console.log(`--- Quantidade de cidade na UF ${_UF} ---`);
+   console.log(`${countCitiesOfState} cidades \n`);
+   console.log(`--- Quantidade de cidades por estado ---`);
+   console.log(`${ JSON.stringify(countCitiesByState.slice(0, 6), null, 2) } \n`);
 }
 
-async function countCities() {
-  const cities = JSON.parse(await fs.readFile(`${folderCities}${_UF}.json`));
+function countCities(uf) {
+  const cities = JSON.parse(fs.readFileSync(`${folderCities}${uf}.json`));
   return cities.length;
 }
 
-async function createFileCity() {
-  const state = JSON.parse(await fs.readFile("Estados.json"));
-  const cities = JSON.parse(await fs.readFile("Cidades.json"));
+function GetArrayCountCitiesOfStates() {
+  let count = [];
+  let countCitiesByState = [];
+  let objState = {};
+
+  for (let i = 0; i < _states.length; i++) {    
+     count = countCities(_states[i].Sigla);
+     objState = { "UF": _states[i].Sigla, "Quantidade": count };
+     countCitiesByState.push(objState);
+  }
+
+  return countCitiesByState.sort((a, b) => {  
+      return b.Quantidade - a.Quantidade;
+  });
+} 
+
+function createFileCity() {
+  _states = JSON.parse(fs.readFileSync("Estados.json"));
+  const cities = JSON.parse(fs.readFileSync("Cidades.json"));
   
-  state.forEach(element => {
+  _states.forEach(element => {
       const cityOfState = cities.filter(cidade => {
           return cidade.Estado === element.ID;
       });
       
-      fs.writeFile(`${folderCities}${element.Sigla}.json`, JSON.stringify(cityOfState, null, 2));
+      fs.writeFileSync(`${folderCities}${element.Sigla}.json`, JSON.stringify(cityOfState, null, 2));
   });
 }
 
